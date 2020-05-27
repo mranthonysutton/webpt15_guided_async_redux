0841d94a90b9affaccdf3f6512189dfd5746060a
Anthony Sutton <anthony.sutton95@gmail.com>
2020-05-26 18:54:45 -0700

    feat: Added boilerplate for redux
    Signed-off-by: Anthony Sutton <anthony.sutton95@gmail.com>

---
+++ client/src/store/reducers/characterReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  characters: [],
+  isFetching: false,
+  errors: "",
+};
+
+export const characterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
